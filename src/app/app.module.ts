3e7c0e7842f98389648e517f726f9f759f481feb
Miguel Bandeira <miguelgbandeira@users.noreply.github.com>
2024-05-21 14:27:07 +0100

    Add shared directory and nav and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { CartComponent } from './cart/cart/cart.component';
 import { ProductsComponent } from './products/products/products.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CartComponent,
-    ProductsComponent
+    ProductsComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
