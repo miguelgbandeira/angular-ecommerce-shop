cd139443c3a540802d6980137c5d8bfcc3de4bf5
Miguel Bandeira <miguelgbandeira@users.noreply.github.com>
2024-05-21 14:22:03 +0100

    Add initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { ProductsComponent } from './products/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
